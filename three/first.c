9375f2fee4422f850289a5dca7a4aeb5745311f2
Austin Sanders <sanderau@oregonstate.edu>
2018-05-13 15:53:43 -0700

    Please update

--- three/first.c
+++ three/first.c
@@ -7,22 +7,30 @@
 //defintions
 #define LOCKED  1
 #define UNLOCKED  0
+#define PROCESSES 8
+#define RESOURCES 2
 
 /***		DATA STRUCTURES 		**/
 
 struct Process {
+	int ID;
 	pthread_t proc; // the actual process
 	struct Resource *curr_resource; // current resource it is using
 };
 
 struct Resource {
+	int ID;
 	struct Process *curr_using[3]; // pointers to the threads using resource
 	int LOCK; //resource locked until all three are done using it
 };
 
+
+struct Process processes[PROCESSES]; // our systems running threads
+struct Resource resources[RESOURCES]; // our systems resources
+
 /*** 		FUNCTIONS 			**/
 
-struct Resource init_resource()
+struct Resource init_resource(int id)
 /***********************************************
  * Name: init_resource
  * Description: Initializes a new resource struct
@@ -32,6 +40,7 @@ struct Resource init_resource()
  * *********************************************/
 {
 	struct Resource r;
+	r.ID = id;
 
 	int i;
 	for(i=0; i<3; i++)
@@ -44,7 +53,7 @@ struct Resource init_resource()
 	return r;
 }
 
-struct Process init_process()
+struct Process init_process(int id)
 /********************************************
  * Name: init_process
  * Description: Initialize a new process structure
@@ -54,6 +63,8 @@ struct Process init_process()
  * *******************************************/
 {
 	struct Process p;
+	p.ID = id;	
+
 	//TODO: bind p_thread
 	
 	p.curr_resource = NULL;
@@ -64,6 +75,12 @@ struct Process init_process()
 /***			MAIN 			***/
 int main(void)
 {
+	int i;
 	
+	for(i=0; i<RESOURCES; i++)
+	{
+		resources[i] = init_resource(i);
+	}	
+
 	return 0;
 }

3f11d4310e3606570b3119c681238be188f30f58
Austin Sanders <sanderau@oregonstate.edu>
2018-06-01 11:10:34 -0700

    initializing the new directory for the final concurrency assignment

---
+++ four/haircut.c
@@ -0,0 +1,14 @@
+//included libraries
+#include <stdlib.h>
+#include <stdio.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(void)
+{
+	printf("Hello world\n");
+
+	return 0;
+}

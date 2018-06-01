0d60143e46fe3ab7b5a8ed0ff37ef46828df7c9f
Austin Sanders <sanderau@oregonstate.edu>
2018-06-01 11:46:44 -0700

    starting to write the haircut program

--- four/haircut.c
+++ four/haircut.c
@@ -6,6 +6,38 @@
 #include <unistd.h>
 #include <time.h>
 
+//definitions
+
+#define N_CHAIRS 8
+#define WORKING 2
+#define ASLEEP 1
+#define AWAKE 0
+
+/*================================================================*/
+
+/***		Data Structures, and global variables 		***/
+
+
+pthread_t threads[N_CHAIRS];
+pthread_mutex_t mut;
+
+struct Barber {
+	int status;
+	int working_chair;
+}typedef Barber;
+
+
+/***		End of Data Structs + Vars			***/
+
+/*===============================================================*/
+
+/***		Functions					***/
+
+
+/***		End of function					***/
+
+
+
 int main(void)
 {
 	printf("Hello world\n");
